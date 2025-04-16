7ed31cd5eab6b41d35bd80cd505f7e1154b8aff8
mnakash <akashmoniruzzaman@gmail.com>
2025-04-16 16:12:16 -0400

    moving helper script to external

--- helper_scripts/handshake.py
+++
@@ -1,9 +0,0 @@
-import stretch_body.robot
-r = stretch_body.robot.Robot()
-did_succeed = False
-while not did_succeed:
-    try:
-        did_succeed = r.startup()
-    finally:
-        r.stop()
-print('successfully connected to Stretch!')
